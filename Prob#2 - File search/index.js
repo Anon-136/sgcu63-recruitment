565abf94831fe637e80479a59d87329f1b4efb0a
Anos Posidos <anonjin44@gmail.com>
2020-06-03 16:10:35 +0700

    [Prob#2] implement depth first search solution

---
+++ Prob#2 - File search/index.js
@@ -0,0 +1,30 @@
+function depthFirstFileSearch(fileToSearch, curObj, curPath = '', depth = 0) {
+	var fileList = []
+	for (const key in curObj) {
+		if (key == '_files') {
+			for (const fileName of curObj[key]) {
+				if (fileName == fileToSearch) {
+					fileList.push({ path: `${curPath}/${fileName}`, depth })
+				}
+			}
+		} else {
+			const result = depthFirstFileSearch(
+				fileToSearch,
+				curObj[key],
+				`${curPath}/${key}`,
+				depth + 1
+			)
+			fileList = fileList.concat(result)
+		}
+	}
+	return fileList
+}
+
+module.exports = function fileSearch(fileToSearch, filesObj) {
+	const obj = JSON.parse(filesObj)
+	const filesObjList = depthFirstFileSearch(fileToSearch, obj)
+	filesObjList.sort((a, b) =>
+		a.depth == b.depth ? a.path > b.path : a.depth > b.depth
+	)
+	return filesObjList.map((fileObj) => fileObj.path)
+}
