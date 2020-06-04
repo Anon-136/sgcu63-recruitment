0ecb42dcecd3cab20eb6ccdae02649b535ceed0e
Anos Posidos <anonjin44@gmail.com>
2020-06-04 09:43:48 +0700

    [Prob#3] refractor & add comment for readability

--- Prob#3 - Numericalization/main.py
+++ Prob#3 - Numericalization/main.py
@@ -3,19 +3,24 @@ n, m1, m2 = inp[0], int(inp[1]), int(inp[2])
 
 display = {}
 with open('display.txt') as file :
-    for i in range(10) :
-        display[i] = []
-        for j in range(5) :
+    for num in range(10) :
+        display[num] = []
+        for row in range(5) :
             line = file.readline()[:5]
-            display[i].append(line)
+            display[num].append(line)
 
 def repeat_print(str, n) :
     print(str * n, end = '')
 
-for i in range(5) :
-    for _ in range(m1) :
-        for num in [int(c) for c in n] :
-            for j in range(5) :
-                repeat_print(display[num][i][j], m2)
-            repeat_print(' ', m2)
-        print(end = '\n')
+# loop each number, reapeatly print each column by m2 times
+def print_row(row) :
+    for num in [int(c) for c in n] :
+        for col in range(5) :
+            repeat_print(display[num][row][col], m2)
+        repeat_print(' ', m2)
+    print(end = '\n')
+
+# loop each row for m1 times
+for row in range(5) :
+    for i in range(m1) :
+        print_row(row)
